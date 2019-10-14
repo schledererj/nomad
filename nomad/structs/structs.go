c7d301cacdac6785c41b9befdf95554ddf1cc7d0
Lang Martin <lang@hashicorp.com>
2019-10-14 15:51:29 -0400

    structs: new CSIVolumeRegisterRequestType, deregister

--- nomad/structs/structs.go
+++ nomad/structs/structs.go
@@ -82,6 +82,8 @@ const (
 	BatchNodeUpdateDrainRequestType
 	SchedulerConfigRequestType
 	NodeBatchDeregisterRequestType
+	CSIVolumeRegisterRequestType
+	CSIVolumeDeregisterRequestType
 )
 
 const (
